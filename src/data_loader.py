57d2d37e361018f39a9989fc5c69996089c82263
akiFQC <yakaredori@gmail.com>
2020-05-31 15:27:39 +0900

    dataloader

---
+++ src/data_loader.py
@@ -0,0 +1,40 @@
+from pprint import pprint
+import os
+import glob
+from matplotlib import pyplot as plt
+import numpy as np
+from pyhdf.SD import SD, SDC  # HDF library module
+import datetime
+import pandas as pd
+
+
+def load(dirpath="../input_data", tile="h27v07"):
+    qpath  =  os.path.join(dirpath, "*."+tile+".006*.hdf")
+    pathes = glob.glob(qpath)
+    print(pathes)
+    file = SD(pathes[0])
+    datasets_dic = file.datasets()
+    keys = list(datasets_dic.keys())
+    df = pd.DataFrame(columns= ["date"]+ keys)
+
+    for pa in pathes:
+        file = SD(pa)
+        datestr = pa.split(".")[-5]
+        datestr = datestr[1:]
+        date = datetime.datetime.strptime(datestr, "%Y%d%m")
+        di = {"date":date}
+        for k in keys:
+            x = np.array(file.select(k)[:,:])
+            di[k] = x
+    
+        df=df.append(di, ignore_index=True)
+    return df
+
+def main():
+    df = load()
+
+    print(df)
+
+
+if __name__=="__main__":
+    main()
